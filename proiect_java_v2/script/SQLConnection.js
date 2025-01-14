9288851e1872149e06b1d80ff2f5434116c509e8
Cozmax <cozmanimic@gmail.com>
2025-01-14 02:37:52 +0200

    Am reusit conexiunea SQL
    Am facut si un basic server side hosting pentru informatii + client side fetching care merge

--- proiect_java_v2/script/SQLConnection.js
+++ proiect_java_v2/script/SQLConnection.js
@@ -3,17 +3,22 @@ const sql = require('mssql');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3000;
+const port = 3000;
 
 app.use(cors());
 app.use(express.json());
 
 const sqlCONFIG = {
-    server: 'localhost',
-    database: 'DESKTOP-87H239E',
+    user: 'Test',
+    password: 'test',
+    server: 'DESKTOP-87H239E',
+    database: 'Proiect_Java',
     options: {
         trustedConnection: true,
         trustServerCertificate: true,
+    },
+    authentication: {
+        type: 'default',
     },
 };
 
@@ -26,7 +31,7 @@ sql.connect(sqlCONFIG)
         console.error('Database connection failed:', err.message);
     });
 
-app.get('/api/data', async (req, res) => {
+app.get('/modeleMasini', async (req, res) => {
     try {
         const pool = await sql.connect(sqlCONFIG);
         const result = await pool.request().query('SELECT * FROM MasiniDisponibile');
@@ -35,4 +40,8 @@ app.get('/api/data', async (req, res) => {
         console.error('Eroare query:', err);
         res.status(500).json({ error: 'Eroare' });
     }
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
--- proiect_java_v2/script/modeleDinStoc.js
+++ proiect_java_v2/script/modeleDinStoc.js
@@ -13,7 +13,24 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
     };
 
+    /* Fetch server-side info :D*/
+    fetch('http://localhost:3000/modeleMasini')
+            .then(response => response.json())
+            .then(data => {
+                const list = document.getElementById('data-list');
+
+                data.forEach(record => {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = JSON.stringify(record);
+                    list.appendChild(listItem);
+                });
+            })
+            .catch(error => {
+                console.error('Eroare:', error);
+            });
+
     
+    /* Acum tot ce trb sa mai fac e sa parse informatiile din JSON si sa le bag intr-o lista de genul \/ */
     const masini = [
         { firma: 'Ford', model: 'Focus', pret: 18000, image: '../imagini/ford-explorer-electric.jpeg' },
     ];
